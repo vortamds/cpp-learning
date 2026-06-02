58e2af44cfe461c3b2f8952c9c93d0af526b9052
vortamds <vortamds@gmail.com>
2026-06-02 14:35:45 +0330

    update

--- main.cpp
+++ main.cpp
@@ -1,27 +1,5 @@
 #include <iostream>
-#include <optional>
-
 int main() {
-    // تست ویژگی C++23: optional monadic operations
-    std::optional<int> opt = 42;
-    
-    auto result = opt.transform([](int x) { 
-        return x * 2; 
-    }).and_then([](int x) -> std::optional<int> {
-        if (x > 50) return x;
-        return std::nullopt;
-    });
-    
-    if (result) {
-        std::cout << "C++23 features work! Result: " << *result << std::endl;
-    } else {
-        std::cout << "C++23 detected but result too small" << std::endl;
-    }
-    
-    // تست C++23: if consteval
-    if consteval {
-        std::cout << "Compiled with C++23 consteval support" << std::endl;
-    }
-    
+    std::cout << "Hello World!";
     return 0;
 }
